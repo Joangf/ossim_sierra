b2c8c0c3fc3ad74073c038286b1a4e0088297495
joangf <nguyenhoang1362005@gmail.com>
2025-04-02 15:08:31 +0700

    Priority Queue Done

--- include/queue.h
+++ include/queue.h
@@ -16,6 +16,11 @@ void enqueue(struct queue_t * q, struct pcb_t * proc);
 struct pcb_t * dequeue(struct queue_t * q);
 
 int empty(struct queue_t * q);
+//SUPPORT FUNCTION
+void swap(struct queue_t *q, int idx1, int idx2);
+void reheapUp(struct queue_t *q, int position);
+void reheapDown(struct queue_t *q, int position);
+
 
 #endif
 
